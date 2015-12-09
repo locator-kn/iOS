5a290a54479de73056ce1f0fee3ff9a1e4aae267
Sergej Birklin <sergej.birklin@skillworks.de>
2015-12-09 17:37:56 +0100

    Added Fonts, first screen

--- locator/locator/WelcomeVC.swift
+++ locator/locator/WelcomeVC.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
+
 
