c27e06776d326308d9cefe6eb4b770ccd5dc5b5f
Michael Knoch <info@michaelknoch.de>
2015-12-15 17:30:46 +0100

    basic gps implementation

--- locator/locator/MapVC.swift
+++ locator/locator/MapVC.swift
@@ -11,11 +11,28 @@ import GoogleMaps
 
 class MapVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let mapView = self.view as! GMSMapView
         mapView.camera = GMSCameraPosition.cameraWithLatitude(-33.8600, longitude: 151.2094, zoom: 10)
+        
+        let locationManager: CLLocationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 20
+        
+        locationManager.startUpdatingLocation()
+    
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        print("update");
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        print(error);
     }
 
   
