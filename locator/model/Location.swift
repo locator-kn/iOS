548c825a3732b243e4c0efb22a3feccc59fade65
Michael Knoch <info@michaelknoch.de>
2015-12-20 11:45:27 +0100

    implement detail constructor

--- locator/model/Location.swift
+++ locator/model/Location.swift
@@ -11,13 +11,26 @@ import Foundation
 class Location {
     
     let id:String
+    let geoPosition:(lat :Double, long :Double)
+
     var title:String
-    var geoPosition:(lat :Double, long :Double)
+    var description:String!
+    var city:String!
+    var imagePath:String!
     
     init(id: String, title: String, long: Double, lat: Double ) {
         self.id = id
         self.title = title
         self.geoPosition = (lat: lat, long: long)
+    }
+    
+    init(id: String, title: String, long: Double, lat: Double, description: String, city:String, imagePath: String) {
+        self.id = id
+        self.title = title
+        self.geoPosition = (lat: lat, long: long)
+        self.description = description
+        self.imagePath = imagePath
+        self.city = city
     }
     
     func getTitle() -> String {
