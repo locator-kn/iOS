5040a9d8050792eb83c7035a009f2b28edd8570e
Michael Knoch <info@michaelknoch.de>
2015-12-21 18:16:20 +0100

    use light statusbar style

--- locator/locator/AppDelegate.swift
+++ locator/locator/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyCAweq9kifxj3KO8lU_Z7oZFZlOO8MgsBQ")
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
         return true
     }
 
