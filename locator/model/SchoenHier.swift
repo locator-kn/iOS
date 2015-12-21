84f8e9e62cb75ea9453545a7fb904c8cc8d2b522
Michael Knoch <info@michaelknoch.de>
2015-12-21 13:42:25 +0100

    use nsdate

--- locator/model/SchoenHier.swift
+++ locator/model/SchoenHier.swift
@@ -11,12 +11,12 @@ import Foundation
 class SchoenHier {
     
     let id:String
-    let createDate: String
+    let createDate: NSDate
     var geoPosition:(lat :Double, long :Double)
     
     init(id: String, createDate:String, long: Double, lat: Double ) {
         self.id = id
-        self.createDate = createDate
+        self.createDate = UtilService.dateFromIso(createDate)
         self.geoPosition = (lat: lat, long: long)
     }
     
