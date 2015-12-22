ee91f4d5d21937fe17da6c63258ad781eba61c55
Michael Knoch <info@michaelknoch.de>
2015-12-22 17:30:02 +0100

    add user and userservice

---
+++ locator/service/UserService.swift
@@ -0,0 +1,13 @@
+//
+//  UserService.swift
+//  locator
+//
+//  Created by Michael Knoch on 22/12/15.
+//  Copyright © 2015 Sergej Birklin. All rights reserved.
+//
+
+import Cocoa
+
+class UserService: NSObject {
+
+}
