a32e97bf89ea7a4bbabd46ff3eae7af104861805
Michael Knoch <info@michaelknoch.de>
2015-12-22 17:30:16 +0100

    implement login

--- locator/service/UserService.swift
+++ locator/service/UserService.swift
@@ -6,8 +6,37 @@
 //  Copyright © 2015 Sergej Birklin. All rights reserved.
 //
 
-import Cocoa
+import Foundation
+import Alamofire
+import PromiseKit
+import SwiftyJSON
 
-class UserService: NSObject {
-
+class UserService {
+    
+    static func login(mail:String, password:String) -> Promise<User> {
+        
+        return Promise { fulfill, reject in
+        
+            Alamofire.request(.POST, "https://locator-app.com/api/v2/users/login", parameters: ["mail": mail, "password": password]).validate().responseJSON {
+                response in
+                
+                switch response.result {
+                case .Success:
+                    
+                    if let value = response.result.value {
+                        let json = JSON(value)
+                            
+                        let id = json["_id"].string
+                        let name = json["name"].string
+                        let email = json["mail"].string
+                       
+                        fulfill(User(id: id!, name: name!, email: email!))
+                    }
+                    
+                case .Failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
 }
