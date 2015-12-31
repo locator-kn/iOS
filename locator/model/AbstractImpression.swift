d730087624a56479886809d4a12c6b29c57fd66d
Michael Knoch <info@michaelknoch.de>
2015-12-31 02:15:14 +0100

    implement abstract impression

--- locator/model/AbstractImpression.swift
+++ locator/model/AbstractImpression.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class AbstractImpression {
+    
+    let id:String
+    let date:NSDate
+    let user:User
+    let data:NSData
+    
+    init(id:String, date:NSDate, user:User, path:String) {
+        self.id = id
+        self.date = date
+        self.user = user
+        self.data = NSData(contentsOfURL: NSURL(string: path)!)!
+    }
+    
+}
