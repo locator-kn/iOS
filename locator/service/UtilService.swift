730a79d187fa59d18073ee9c9e4d554fdb373bf3
Michael Knoch <info@michaelknoch.de>
2016-01-05 20:41:37 +0100

    implement distanceBetweenCoords

--- locator/service/UtilService.swift
+++ locator/service/UtilService.swift
@@ -16,4 +16,22 @@ class UtilService {
         return formatter.dateFromString(iso)!
     }
     
+    static func distanceBetweenCoords(lat1:Double, lon1:Double, lat2:Double, lon2:Double) -> Double {
+        let R = 6371000.0; // metres
+        let _lat1 = toRadian(lat1);
+        let _lat2 = toRadian(lat2);
+        let delta1 = toRadian(lat2-lat1);
+        let delta2 = toRadian(lon2-lon1);
+        
+        let a = sin(delta1/2) * sin(delta1/2) + cos(_lat1) * cos(_lat2) * sin(delta2/2) * sin(delta2/2);
+        let c = 2 * atan2(sqrt(a), sqrt(1-a));
+        
+        let d = R * c;
+        return d
+    }
+    
+    static func toRadian(num:Double) -> Double {
+        return num * M_PI / 180;
+    }
+    
 }
