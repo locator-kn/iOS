79e71410d6369c018b188309defd5cdce2af5420
Michael Knoch <info@michaelknoch.de>
2016-01-08 14:17:08 +0100

    implement imagepicker

--- locator/controller/ImageImpressionVC.swift
+++ locator/controller/ImageImpressionVC.swift
@@ -8,28 +8,51 @@
 
 import UIKit
 
-class ImageImpressionVC: UIViewController {
+class ImageImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    var image:UIImage?
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // camera configuration
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if image == nil {
+            self.takePicture()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            image = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            image = possibleImage
+        } else {
+            return
+        }
+        imageView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func takePicture() {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
 
 }
