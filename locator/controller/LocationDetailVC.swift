c678a89ea3562a260794889e152131ee4fb0c6af
Michael Knoch <info@michaelknoch.de>
2016-01-08 18:51:06 +0100

    prepare segue with locationID

--- locator/controller/LocationDetailVC.swift
+++ locator/controller/LocationDetailVC.swift
@@ -85,14 +85,12 @@ class LocationDetailVC: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        print(segue.identifier)
+        if (segue.identifier == "textImpression") {
+            let controller = segue.destinationViewController as! TextImpressionVC
+            controller.locationId = self.location.id
+        }
     }
-    */
 
 }
