1f72860f71b7b5f4e27304465e2dffb9c6113b0c
Michael Knoch <info@michaelknoch.de>
2016-01-13 09:58:09 +0100

    add protected request

--- locator/service/UserService.swift
+++ locator/service/UserService.swift
@@ -39,4 +39,22 @@ class UserService {
             }
         }
     }
+    
+
+    static func protected() -> Promise<Bool> {
+        
+        return Promise { fulfill, reject in
+            
+            Alamofire.request(.GET, "https://locator-app.com/api/v2/users/protected").validate().responseJSON {
+                response in
+                
+                switch response.result {
+                case .Success:
+                    fulfill(true)
+                case .Failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
 }
