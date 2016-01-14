82c79c2fa9692b937c284ec21891809d7e9ead43
Michael Knoch <info@michaelknoch.de>
2016-01-14 16:01:34 +0100

    fix imageimpression post

--- locator/service/LocationService.swift
+++ locator/service/LocationService.swift
@@ -233,7 +233,7 @@ class LocationService {
     static func addImageImpression(id: String, data:UIImage) -> Promise<Bool> {
         return Promise { fulfill, reject in
             
-            Alamofire.upload(.POST, "https://locator-app.com/api/v2/locations/" + id + "/stream/image", data: UIImageJPEGRepresentation(data, 1.0)!, name: "file").validate().responseJSON {
+            Alamofire.upload(.POST, "https://locator-app.com/api/v2/locations/" + id + "/stream/image", data: UIImageJPEGRepresentation(data, 1.0)!).validate().responseJSON {
                 response in
                 
                 print(data)
