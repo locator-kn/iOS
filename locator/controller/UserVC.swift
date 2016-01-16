b5e22b5271ddf9b614a26498b976e6be4175f094
Michael Knoch <info@michaelknoch.de>
2016-01-16 13:56:10 +0100

    add empty uservc

--- locator/controller/UserVC.swift
+++ locator/controller/UserVC.swift
@@ -19,6 +19,8 @@ class UserVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        UITabBar.appearance().barTintColor = UIColor.whiteColor()
     }
     
 
