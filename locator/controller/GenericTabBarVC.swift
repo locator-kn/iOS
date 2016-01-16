1f540092240fea5cf64a7e575ffb860e3474cd13
Michael Knoch <info@michaelknoch.de>
2016-01-16 13:55:44 +0100

    add generictabbarvc for styling purpose

---
+++ locator/controller/GenericTabBarVC.swift
@@ -0,0 +1,35 @@
+//
+//  GenericTabBarVC.swift
+//  locator
+//
+//  Created by Michael Knoch on 16/01/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import UIKit
+
+class GenericTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().barTintColor = UIColor.blackColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
