8a4405cd2377d7a304b2f0fc0aa3dc2a478c0df1
Michael Knoch <info@michaelknoch.de>
2016-01-18 18:06:15 +0100

    implement locationCell

---
+++ locator/locator/LocationCell.swift
@@ -0,0 +1,28 @@
+//
+//  LocationCell.swift
+//  locator
+//
+//  Created by Michael Knoch on 18/01/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+   
+    @IBOutlet weak var locationTitle: UILabel!
+    @IBOutlet weak var locationImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
