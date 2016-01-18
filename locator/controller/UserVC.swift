762ab958a20c7315762c63e9caa530736450a307
Michael Knoch <info@michaelknoch.de>
2016-01-18 10:42:53 +0100

    add outlet references

--- locator/controller/UserVC.swift
+++ locator/controller/UserVC.swift
@@ -13,8 +13,9 @@ class UserVC: UIViewController {
     var user:User = User(id: "56786fe35227864133663978")
     var userLocations: [Location]?
     
-    //keep UserTabVC reference
-    var tabVC:UserTabVC?
+    //tab references
+    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var subView: UIView!
     
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var userName: UILabel!
@@ -22,6 +23,13 @@ class UserVC: UIViewController {
     override func viewDidLoad() {
         print("userid", self.user.id)
         super.viewDidLoad()
+        
+        // set new blank images as background and shadow, great hack, like this.
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().shadowImage = UIImage()
+        
+        // font and fontsize
+        UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName:UIFont(name: "Source Sans Pro", size: 16)!], forState: .Normal)
         
         // fetch user information
         UserService.getUser(user.id!)
@@ -71,9 +79,7 @@ class UserVC: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "tabBarVC") {
-            self.tabVC = segue.destinationViewController as? UserTabVC
-        }
+        
     }
 
 }
