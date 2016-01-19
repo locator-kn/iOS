e977198ccf0fe92bb532b99a435b4d5a0ba6f029
Michael Knoch <info@michaelknoch.de>
2016-01-19 11:55:17 +0100

    implement city

---
+++ locator/model/City.swift
@@ -0,0 +1,19 @@
+//
+//  City.swift
+//  locator
+//
+//  Created by Michael Knoch on 19/01/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import Foundation
+
+class City {
+    let id:String
+    let title:String
+    
+    init(id:String, title:String) {
+        self.id = id
+        self.title = title
+    }
+}
