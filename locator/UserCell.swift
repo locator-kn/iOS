848369a35a674c04273c17f0e236774df61e6a16
Michael Knoch <info@michaelknoch.de>
2016-01-20 11:31:39 +0100

    add new tableview files

---
+++ locator/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  locator
+//
+//  Created by Michael Knoch on 20/01/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
