40c650030b87686b94ce681b4493fdd7ac33260d
Sergej Birklin <sergej.birklin@skillworks.de>
2016-01-24 12:43:50 +0100

    added background screen for bubble view

--- locator/service/BubbleVC.swift
+++ locator/service/BubbleVC.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class BubbleVC: UIViewController {
     
+    // screen width and height:
+    let width = UIScreen.mainScreen().bounds.size.width
+    let height = UIScreen.mainScreen().bounds.size.height
+    
+    let backgroundImage = UIImage(named: "Background-rot.png")
+    
     var bubble = BubbleView(frame: CGRect(x: 10, y: 100, width: 100, height: 100))
+    //let backgroundImageView = UIImageView(image: UIImage(named: "Background-rot.png"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let backgroundImageView = UIImageView(frame: CGRectMake(0, 0, width, height))
+        backgroundImageView.image = backgroundImage
+        backgroundImageView.contentMode = .ScaleAspectFit
+        view.addSubview(backgroundImageView)
         view.addSubview(bubble)
     }
 
@@ -22,6 +32,11 @@ class BubbleVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
     
     
 
