a9899cace6a5bd2b6006bd0209dbc67cfd4537e8
Michael Knoch <info@michaelknoch.de>
2016-01-30 23:25:04 +0100

    implement device magic

--- locator/service/DeviceService.swift
+++ locator/service/DeviceService.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+import PromiseKit
+import SwiftyJSON
+
+class DeviceService {
+    
+    static func getDeviceData() -> [String: String] {
+        var deviceData = [String: String]()
+        deviceData["deviceId"] = UIDevice.currentDevice().identifierForVendor!.UUIDString
+        deviceData["manufacturer"] = "apple"
+        deviceData["deviceModel"] = UIDevice.currentDevice().model
+        deviceData["version"] = UIDevice.currentDevice().systemVersion
+        deviceData["type"] = "ios"
+        return deviceData
+    }
+
+    static func registerDevice(deviceData: [String:String]) -> Promise<Bool> {
+        
+        return Promise { fulfill, reject in
+            
+            Alamofire.request(.POST, API.BASE_URL + "/device/register", parameters: deviceData).validate().responseJSON {
+                response in
+                
+                switch response.result {
+                case .Success:
+                    fulfill(true)
+                case .Failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+
+}
+
