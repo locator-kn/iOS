72d00539920ea9d2af995f0da5cc71f1218252d0
Timo Weiß <info@timo-weiss.com>
2016-02-23 09:43:59 +0100

    add user to participant

--- locator/model/Participant.swift
+++ locator/model/Participant.swift
@@ -2,7 +2,16 @@
 import Foundation
 
 class Participant {
-    init(user_id:String, last_read:Int) {
-        
+    
+    let user_id:String
+    let last_read:Int
+    let user:User
+    
+
+    
+    init(user_id:String, last_read:Int, user:User) {
+        self.user_id = user_id
+        self.last_read = last_read
+        self.user = user
     }
 }
