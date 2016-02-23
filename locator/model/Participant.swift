61acc13a4851a54bf10fc7ab16aa6a1b56656c38
Timo Weiß <info@timo-weiss.com>
2016-02-23 12:21:35 +0100

    remove user from init

--- locator/model/Participant.swift
+++ locator/model/Participant.swift
@@ -5,13 +5,12 @@ class Participant {
     
     let user_id:String
     let last_read:Int
-    let user:User
     
 
     
-    init(user_id:String, last_read:Int, user:User) {
+    init(user_id:String, last_read:Int) {
         self.user_id = user_id
         self.last_read = last_read
-        self.user = user
+        
     }
 }
