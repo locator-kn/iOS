037fccc7de968315a64b9f571cebe34bee0a490f
Timo Weiß <info@timo-weiss.com>
2016-02-24 10:19:45 +0100

    add last_message to conversation

--- locator/model/Conversation.swift
+++ locator/model/Conversation.swift
@@ -5,9 +5,11 @@ class Conversation {
     
     let _id:String
     let participants:[Participant]
+    var last_message:Message
     
-    init(_id: String, participants:[Participant]) {
+    init(_id: String, participants:[Participant], last_message:Message) {
         self._id = _id
         self.participants = participants
+        self.last_message = last_message
     }
 }
