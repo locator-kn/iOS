ac746db22b91b3ec9352475bfc2971960b6cca15
Michael Knoch <info@michaelknoch.de>
2016-02-24 16:09:55 +0100

    add tableview and cells

---
+++ locator/locator/TextImpressionCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextImpression.swift
+//  locator
+//
+//  Created by Michael Knoch on 24/02/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import UIKit
+
+class TextImpression: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
