635e712bf8ebfd6df98a4ff464d725cdb70d0d84
Timo Weiß <info@timo-weiss.com>
2016-02-24 16:15:16 +0100

    add user object optionally

--- locator/model/Participant.swift
+++ locator/model/Participant.swift
@@ -5,12 +5,18 @@ class Participant {
     
     let user_id:String
     let last_read:Int
+    var user:User?
     
 
+    
+    init(user_id:String, last_read:Int, user:User) {
+        self.user_id = user_id
+        self.last_read = last_read
+        self.user = user
+    }
     
     init(user_id:String, last_read:Int) {
         self.user_id = user_id
         self.last_read = last_read
-        
     }
 }
