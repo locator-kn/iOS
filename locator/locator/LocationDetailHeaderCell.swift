4c9abe57d5278b1ccf27bbd10550e25fb18cebce
Michael Knoch <info@michaelknoch.de>
2016-02-25 19:35:43 +0100

    add toggle to favor location

---
+++ locator/locator/LocationDetailHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationDetailHeaderCell.swift
+//  locator
+//
+//  Created by Michael Knoch on 25/02/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
