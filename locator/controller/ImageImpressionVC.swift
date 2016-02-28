13416b7b50ee2559ef3c3ac5d916d5c972b2228b
Michael Knoch <info@michaelknoch.de>
2016-02-28 18:11:32 +0100

    implement fusuma

--- locator/controller/ImageImpressionVC.swift
+++ locator/controller/ImageImpressionVC.swift
@@ -10,99 +10,72 @@ import UIKit
 import MediaPlayer
 import MobileCoreServices
 import AVFoundation
+import Fusuma
 
-class ImageImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, FusumaDelegate {
 
     var locationId:String!
+    var initial = false
     let imagePicker = UIImagePickerController()
     
     var image:UIImage?
-    var videoData:NSData?
     var video:Bool = false
-    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
+        self.view.alpha = 0
         super.viewDidLoad()
-        
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            // camera configuration
-            imagePicker.delegate = self
-            imagePicker.allowsEditing = true
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
-            if video {
-                imagePicker.mediaTypes = [kUTTypeMovie as String]
-            }
-        }
-        
-    }
-    @IBAction func close(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
-        if image == nil && videoData == nil {
-            self.takePicture()
+        if image == nil && !self.initial {
+            self.openCamera()
         }
+    }
+    
+    func openCamera() {
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        self.presentViewController(fusuma, animated: true, completion: nil)
+        self.initial = true
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
+    func fusumaDismissedWithImage(image: UIImage) {
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        
-        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
-            image = possibleImage
-        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
-            image = possibleImage
-        } else if let possibleVideo = info[UIImagePickerControllerMediaURL] as? NSURL! {
-            let tempImage = possibleVideo
-            print(tempImage)
-            videoData = NSData(contentsOfURL: tempImage)
-        }
-        imageView.image = image
-        dismissViewControllerAnimated(true, completion: nil)
-        
-        self.submitImage(self)
+    func fusumaImageSelected(image: UIImage) {
+        self.image = image
+        self.imageView.image = self.image
+        self.view.alpha = 1
+    }
+    
+    func fusumaCameraRollUnauthorized() {
+        AlertService.simpleAlert(self, message: "Wir benötigen Rechte um deine Kamera zu nutzen")
+    }
 
-    }
-    
-    func addFilter(rawImage:UIImage) -> UIImage {
-        let filter = CIFilter(name: "CIColorControls")!
-        filter.setValue(CIImage(image: rawImage), forKey: kCIInputImageKey)
-        filter.setValue(0.5, forKey: kCIInputIntensityKey)
-        return UIImage(CIImage: filter.outputImage!)
-    }
-    
-    func takePicture() {
-        presentViewController(imagePicker, animated: false, completion: nil)
+    @IBAction func noeButton(sender: AnyObject) {
+        
     }
     
     @IBAction func submitImage(sender: AnyObject) {
-        
-        if !video {
-            ImpressionService.addImageImpression(self.locationId, data: image!).then{
-                result -> Void in
-                print("image upload success")
-            }.always {
-                self.dismissViewControllerAnimated(true, completion: nil);
-            }
-        } else {
-            print(videoData!.length)
-            ImpressionService.addVideoImpression(self.locationId, data: videoData!).then{
-                result -> Void in
-                print("video upload success")
-            }.always {
-                print("always called")
-                self.dismissViewControllerAnimated(true, completion: nil);
-            }
+        ImpressionService.addImageImpression(self.locationId, data: image!).then{
+            result -> Void in
+            print("image upload success")
+        }.always {
+            self.dismissViewControllerAnimated(true, completion: nil);
         }
-        
+    }
+    
+    @IBAction func close(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
