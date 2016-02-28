a968e551410b10a5b0ab0e1f26dbfb0a0dffa62f
Sergej Birklin <Sergej@sergejs-macbook-pro.local>
2016-02-28 11:16:43 +0100

    Added first Bubbles without algo

--- locator/service/BubbleVC.swift
+++ locator/service/BubbleVC.swift
@@ -10,19 +10,42 @@ import UIKit
 
 class BubbleVC: UIViewController {
     
-    let long = 9.169753789901733
-    let lat = 47.66868204997508
-    let maxDistance: Float = 2.0
-    let limit = 20
-
+//    let long = 9.169753789901733
+//    let lat = 47.66868204997508
+//    let maxDistance: Float = 2.0
+//    let limit = 20
+    let colorRed = Color.red()
+    
+    // Schoenhier
+    @IBOutlet weak var schoenHierImageView: UIImageView!
+    // UserProfil
+    // First Bubble
+    @IBOutlet weak var firstBubbleImageView: UIImageView!
+    // Second Bubble
+    @IBOutlet weak var secondBubbleImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        BubbleService.getBubbles(lat, long: long, maxDistance: maxDistance, limit: limit).then { bubbles -> Void in
-            
-        }
-            .then {
-                self.reloadInputViews()
-        }
+//        BubbleService.getBubbles(lat, long: long, maxDistance: maxDistance, limit: limit).then { bubbles -> Void in
+//            
+//            }
+//            .then {
+//                self.reloadInputViews()
+//        }
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layoutBubbles(firstBubbleImageView)
+        layoutBubbles(secondBubbleImageView)
+    }
+    
+    func layoutBubbles(imageView: UIImageView) {
+        imageView.layer.cornerRadius = imageView.frame.width / 2
+        imageView.clipsToBounds = true
+        imageView.layer.borderColor = colorRed.first.CGColor
+        imageView.layer.borderWidth = 4
+    }
+    
 }
