62e00c581de51d3a733c1f045edb4899775c1184
Michael Knoch <info@michaelknoch.de>
2016-02-28 18:12:30 +0100

    copy from image

--- locator/locator/VideoImpressionVC.swift
+++ locator/locator/VideoImpressionVC.swift
@@ -1,35 +1,119 @@
 //
-//  VideoImpressionVC.swift
+//  ImageImpressionVC.swift
 //  locator
 //
-//  Created by Michael Knoch on 28/02/16.
-//  Copyright © 2016 Locator. All rights reserved.
+//  Created by Michael Knoch on 08/01/16.
+//  Copyright © 2016 Sergej Birklin. All rights reserved.
 //
 
 import UIKit
+import MediaPlayer
+import MobileCoreServices
+import AVFoundation
+import Fusuma
 
-class VideoImpressionVC: UIViewController {
-
+class VideoImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var locationId:String!
+    let imagePicker = UIImagePickerController()
+    
+    var image:UIImage?
+    var videoData:NSData?
+    var video:Bool = false
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+        // camera configuration
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+        if video {
+        imagePicker.mediaTypes = [kUTTypeMovie as String]
+        }
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func close(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(animated: Bool) {
+        if image == nil && videoData == nil {
+            self.takePicture()
+        }
     }
-    */
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    
+    if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+    image = possibleImage
+    } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+    image = possibleImage
+    } else if let possibleVideo = info[UIImagePickerControllerMediaURL] as? NSURL! {
+    let tempImage = possibleVideo
+    print(tempImage)
+    videoData = NSData(contentsOfURL: tempImage)
+    }
+    imageView.image = image
+    dismissViewControllerAnimated(true, completion: nil)
+    
+    self.submitImage(self)
+    
+    }*/
+    
+    func takePicture() {
+        presentViewController(imagePicker, animated: false, completion: nil)
+    }
+    
+    func fusumaImageSelected(image: UIImage) {
+        
+        print("Image selected")
+    }
+    
+    // Return the image but called after is dismissed.
+    func fusumaDismissedWithImage(image: UIImage) {
+        
+        print("Called just after FusumaViewController is dismissed.")
+    }
+    
+    // When camera roll is not authorized, this method is called.
+    func fusumaCameraRollUnauthorized() {
+        
+        print("Camera roll unauthorized")
+    }
+    
+    @IBAction func submitImage(sender: AnyObject) {
+        
+        if !video {
+            ImpressionService.addImageImpression(self.locationId, data: image!).then{
+                result -> Void in
+                print("image upload success")
+                }.always {
+                    self.dismissViewControllerAnimated(true, completion: nil);
+            }
+        } else {
+            print(videoData!.length)
+            ImpressionService.addVideoImpression(self.locationId, data: videoData!).then{
+                result -> Void in
+                print("video upload success")
+                }.always {
+                    print("always called")
+                    self.dismissViewControllerAnimated(true, completion: nil);
+            }
+        }
+        
+    }
+    
 }
