39c72a923010c19ca4444c849d2f4e60af72a923
Michael Knoch <info@michaelknoch.de>
2016-02-28 18:26:42 +0100

    implement standalone videopicker

--- locator/locator/VideoImpressionVC.swift
+++ locator/locator/VideoImpressionVC.swift
@@ -16,34 +16,20 @@ class VideoImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINa
     
     var locationId:String!
     let imagePicker = UIImagePickerController()
-    
-    var image:UIImage?
     var videoData:NSData?
     var video:Bool = false
-    
-    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-        // camera configuration
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
-        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
-        if video {
-        imagePicker.mediaTypes = [kUTTypeMovie as String]
+            imagePicker.mediaTypes = [kUTTypeMovie as String]
         }
-        }
-        
-    }
-    @IBAction func close(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
-        if image == nil && videoData == nil {
-            self.takePicture()
+        if videoData == nil {
+            self.openPicker()
         }
     }
     
@@ -51,67 +37,33 @@ class VideoImpressionVC: UIViewController, UIImagePickerControllerDelegate, UINa
         super.didReceiveMemoryWarning()
     }
     
-    /*func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-    dismissViewControllerAnimated(true, completion: nil)
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
     
-    if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
-    image = possibleImage
-    } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
-    image = possibleImage
-    } else if let possibleVideo = info[UIImagePickerControllerMediaURL] as? NSURL! {
-    let tempImage = possibleVideo
-    print(tempImage)
-    videoData = NSData(contentsOfURL: tempImage)
+        if let possibleVideo = info[UIImagePickerControllerMediaURL] as? NSURL! {
+            let tempImage = possibleVideo
+            print(tempImage)
+            videoData = NSData(contentsOfURL: tempImage)
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+        self.submitVideo()
+    
     }
-    imageView.image = image
-    dismissViewControllerAnimated(true, completion: nil)
     
-    self.submitImage(self)
-    
-    }*/
-    
-    func takePicture() {
+    func openPicker() {
         presentViewController(imagePicker, animated: false, completion: nil)
     }
     
-    func fusumaImageSelected(image: UIImage) {
-        
-        print("Image selected")
-    }
-    
-    // Return the image but called after is dismissed.
-    func fusumaDismissedWithImage(image: UIImage) {
-        
-        print("Called just after FusumaViewController is dismissed.")
-    }
-    
-    // When camera roll is not authorized, this method is called.
-    func fusumaCameraRollUnauthorized() {
-        
-        print("Camera roll unauthorized")
-    }
-    
-    @IBAction func submitImage(sender: AnyObject) {
-        
-        if !video {
-            ImpressionService.addImageImpression(self.locationId, data: image!).then{
-                result -> Void in
-                print("image upload success")
-                }.always {
-                    self.dismissViewControllerAnimated(true, completion: nil);
-            }
-        } else {
-            print(videoData!.length)
-            ImpressionService.addVideoImpression(self.locationId, data: videoData!).then{
-                result -> Void in
-                print("video upload success")
-                }.always {
-                    print("always called")
-                    self.dismissViewControllerAnimated(true, completion: nil);
-            }
+    func submitVideo() {
+        ImpressionService.addVideoImpression(self.locationId, data: videoData!).then{
+            result -> Void in
+            print("video upload success")
+        }.always {
+            print("always called")
+            self.dismissViewControllerAnimated(true, completion: nil);
         }
         
     }
