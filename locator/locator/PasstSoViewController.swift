06d11cbb03095a95aecef9e00a93f03209a3980f
Timo Weiß <info@timo-weiss.com>
2016-02-29 16:36:22 +0100

    init

---
+++ locator/locator/PasstSoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PasstSoViewController.swift
+//  locator
+//
+//  Created by Timo Weiß on 29.02.16.
+//  Copyright © 2016 Locator. All rights reserved.
+//
+
+import UIKit
+
+class PasstSoViewController: UIViewController {
+
+    @IBOutlet weak var uiImageView: UIImageView!
+    
+    var uiimage:UIImage!
+    
+    var lat:Double!
+    var long:Double!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("lalala")
+        
+        uiImageView.image = uiimage
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
