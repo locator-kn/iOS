3573f72965cfce3c58119ff8dd2cf1d459a50575
Michael Knoch <info@michaelknoch.de>
2016-03-01 17:35:43 +0100

    implement hide

--- locator/customViews/BubbleView.swift
+++ locator/customViews/BubbleView.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import PromiseKit
 
 class BubbleView: UIImageView {
 
     required init?(coder aDecoder: NSCoder)  {
         super.init(coder: aDecoder)
         self.alpha = 0
-        self.transform = CGAffineTransformMakeScale(2, 2)
+        self.transform = CGAffineTransformMakeScale(0.2, 0.2)
     }
 
     func show() {
@@ -25,6 +26,26 @@ class BubbleView: UIImageView {
                 self.alpha = 1
                 self.transform = CGAffineTransformMakeScale(1, 1)
         }, completion: nil)
+    }
+    
+    func hide() -> Promise<Bool> {
+        
+        return Promise {
+            fulfill, reject in
+            let delay = Double(arc4random_uniform(8)) * 0.1 + 0.3
+            UIView.animateWithDuration(delay * 0.3,
+                delay: delay,
+                options: .CurveEaseOut,
+                animations: {
+                    self.alpha = 0
+                    self.transform = CGAffineTransformMakeScale(2, 2)
+                },
+                completion: {
+                    void in
+                    self.transform = CGAffineTransformMakeScale(0.2, 0.2)
+                    fulfill(true)
+                })
+        }
     }
 
 }
