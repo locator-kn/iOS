fc70221cf01d812158fdd8d6df792856d5247fac
Michael Knoch <info@michaelknoch.de>
2016-03-02 14:59:11 +0100

    implement playvideo

--- locator/locator/VideoImpressionCell.swift
+++ locator/locator/VideoImpressionCell.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoImpressionCell: UITableViewCell {
 
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userThumb: UIImageView!
+    
+    var player: AVPlayer?
+    var playerController: AVPlayerViewController?
+    var vc: LocationDetailVC!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func clickThumb(sender: UIButton) {
+    
+        func playVideo() {
+            
+            let myBaseUrl = "https://locator-app.com/api/v2/locations/impression/video/56b907022f08ffcd26f8c13a/impression.mov"
+            if let url = NSURL(string: myBaseUrl) {
+                
+                self.player = AVPlayer(URL: url)
+                self.playerController = AVPlayerViewController()
+                self.playerController!.player = self.player
+                
+                self.vc.presentViewController(self.playerController!, animated: true) {
+                    () -> Void in
+                    self.playerController!.player?.play()
+                }
+            }
+            
+        }
+    
+    }
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
