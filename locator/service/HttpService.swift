12ec8e821ce50cc3d61522e5b31be8e711edde3f
Sergej Birklin <sergej.birklin@htwg-konstanz.de>
2016-03-03 17:58:59 +0100

    added profilimage upload during registration

--- locator/service/HttpService.swift
+++ locator/service/HttpService.swift
@@ -55,6 +55,7 @@ struct API {
     static let USER_LOGOUT = "\(BASE_URL)/users/logout"
     static let USER_PROTECTED = "\(BASE_URL)/users/protected"
     static let BUBBLE_INPUT = "\(BASE_URL)/my/bubblescreen"
+    static let PROFIL_IMAGE_UPLOAD = "\(BASE_URL)/users/image"
 }
 
 
