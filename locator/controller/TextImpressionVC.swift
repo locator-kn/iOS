4b34871764fd0d8392721967eb9cfc83571c13ea
Michael Knoch <info@michaelknoch.de>
2016-03-07 00:50:57 +0100

    add loading spinner to prevent duplicate text impressions, final versionbump

--- locator/controller/TextImpressionVC.swift
+++ locator/controller/TextImpressionVC.swift
@@ -13,6 +13,7 @@ class TextImpressionVC: UIViewController, UITextFieldDelegate {
     var locationId:String!
     var vc: LocationDetailVC!
     @IBOutlet weak var textField: UITextField!
+    var loader: LoadingView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,11 +39,16 @@ class TextImpressionVC: UIViewController, UITextFieldDelegate {
         
         if textField.text?.characters.count > 3 {
             
+            self.loader = LoadingView(frame: self.view.frame)
+            self.loader.backgroundColor = COLORS.yellow
+            self.view.addSubview(loader)
+            
             ImpressionService.addTextImpression(self.locationId, data: textField.text!).then {
                 result -> Void in
                 print("post textimpression success")
                 self.vc.loadData()
             }.always {
+                self.loader.dismiss()
                 self.dismissViewControllerAnimated(true, completion: nil);
             }
         } else {
