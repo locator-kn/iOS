7c8c7bac68a7f527bd02dcbdb664676687302f71
Michael Knoch <info@michaelknoch.de>
2016-04-16 15:28:39 +0200

    implement camera helpers

--- locator/service/CameraService.swift
+++ locator/service/CameraService.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import UIKit
+
+class CameraService {
+    
+    static func uploadProfileImage(ctrl: UIViewController, imageSource: UIImagePickerController) {
+        
+        let alert = UIAlertController(title: "Profilbild ändern", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+            captureImage(ctrl, imageSource: imageSource)
+        }))
+        alert.addAction(UIAlertAction(title: "Gallery", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+            getFromGallery(ctrl, imageSource: imageSource)
+        }))
+        ctrl.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    static private func captureImage(ctrl: UIViewController, imageSource: UIImagePickerController) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            imageSource.sourceType = UIImagePickerControllerSourceType.Camera
+            ctrl.presentViewController(imageSource, animated: true) {}
+        }
+    }
+    
+    static private func getFromGallery(ctrl: UIViewController, imageSource: UIImagePickerController) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            imageSource.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            ctrl.presentViewController(imageSource, animated: true) {}
+        }
+    }
+    
+}
