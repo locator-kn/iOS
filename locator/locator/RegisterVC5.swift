e3835dfb7a51ff208a645af31b89b6194eb167b0
Michael Knoch <info@michaelknoch.de>
2016-04-16 15:29:12 +0200

    refactor nearly everything

--- locator/locator/RegisterVC5.swift
+++ locator/locator/RegisterVC5.swift
@@ -24,23 +24,18 @@ class RegisterVC5: UIViewController, UINavigationControllerDelegate, UIImagePick
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         imageFromSource.delegate = self
-        imageFromSource.allowsEditing = true
-        
         let tapGestureRecognizer1 = UITapGestureRecognizer(target:self, action:#selector(self.imageTapped(_:)))
         profilImageView.addGestureRecognizer(tapGestureRecognizer1)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        profilImageView.layer.cornerRadius = profilImageView.frame.size.width / 2
-        profilImageView.clipsToBounds = true
+        profilImageView = UtilService.roundImageView(profilImageView)
     }
     
     override func prefersStatusBarHidden() -> Bool {
@@ -52,30 +47,25 @@ class RegisterVC5: UIViewController, UINavigationControllerDelegate, UIImagePick
     }
     
     func yesButtonTapped(sender: UIImageView) {
+        
         if profilImageChanged == true {
-            Alamofire.upload(
-                .POST,
-                API.PROFIL_IMAGE_UPLOAD,
-                multipartFormData: { multipartFormData in
-                    multipartFormData.appendBodyPart(data: UIImageJPEGRepresentation(self.profilImageView.image!, 0.5)!, name: "file", fileName: "userProfilImage.jpg", mimeType: "image/jpeg")
-                },
-                encodingCompletion: { encodingResult in
-                    switch encodingResult {
-                    case .Success( _, _, _):
-                        self.performSegueWithIdentifier("showPreLogin", sender: self)
-                    case .Failure(let encodingError):
-                        self.alertFalseImageUpload()
-                        print(encodingError)
-                    }
+            
+            UserService.uploadProfileImg(self.profilImageView.image!)
+                .then {
+                    result -> Void in
+                    self.performSegueWithIdentifier("showPreLogin", sender: self)
                 }
-            )
+                .error {
+                    err -> Void in
+                    self.alertFalseImageUpload()
+            }
+            
         } else {
             alertNoImageSelected()
         }
     }
     
-    func imageTapped(img: AnyObject)
-    {
+    func imageTapped(img: AnyObject) {
         let alert = UIAlertController(title: "Select Image", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
         
         alert.addAction(UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
@@ -88,26 +78,21 @@ class RegisterVC5: UIViewController, UINavigationControllerDelegate, UIImagePick
     }
     
     func alertFalseImageUpload() {
-        let alert = UIAlertController(title: "Ups", message: "Dein Profilbild konnte nicht geladen werden!", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
+        AlertService.simpleAlert(self, message: "Dein Profilbild konnte nicht geladen werden!")
     }
     
     func alertNoImageSelected() {
-        let alert = UIAlertController(title: "Ups", message: "Du musst ein Profilbild auswählen!", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
+        AlertService.simpleAlert(self, message: "Du musst ein Profilbild auswählen!")
     }
     
-    @IBAction func captureImage() {
-        
+    func captureImage() {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
             imageFromSource.sourceType = UIImagePickerControllerSourceType.Camera
             self.presentViewController(imageFromSource, animated: true) {}
         }
     }
     
-    @IBAction func getFromGallery()  {
+    func getFromGallery()  {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
             imageFromSource.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
             self.presentViewController(imageFromSource, animated: true) {}
@@ -115,20 +100,10 @@ class RegisterVC5: UIViewController, UINavigationControllerDelegate, UIImagePick
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        let tmp: UIImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        let tmp: UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         profilImageChanged = true
         profilImageView.image = tmp
         self.dismissViewControllerAnimated(true) { () -> Void in }
     }
     
 }
-
-/*
-// MARK: - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-// Get the new view controller using segue.destinationViewController.
-// Pass the selected object to the new view controller.
-}
-*/
