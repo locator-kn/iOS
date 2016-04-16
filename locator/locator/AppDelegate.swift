f91ea35971623ade6300bc27b6ad474685edf04f
Michael Knoch <info@michaelknoch.de>
2016-04-16 12:37:05 +0200

    trigger bubblefetch on change state

--- locator/locator/AppDelegate.swift
+++ locator/locator/AppDelegate.swift
@@ -70,12 +70,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        if let ctrl = window!.rootViewController?.childViewControllers.last as? BubbleVC {
+            ctrl.resolveBubblePromises()
+        }
         TrackingService.sharedInstance.trackEvent("App | launch")
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         FBSDKAppEvents.activateApp()
     }
 
