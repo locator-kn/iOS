ef634fa5cc3b1c5a1e836ef8324d569cbe359399
Michael Knoch <info@michaelknoch.de>
2016-05-03 01:52:36 +0200

    Revert "remove initial push registration"
    This reverts commit 87055fcbf890f30267d0a572e91e2e739b3d24ab.

--- locator/locator/AppDelegate.swift
+++ locator/locator/AppDelegate.swift
@@ -59,6 +59,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             TrackingService.sharedInstance.trackEvent("App | launch as guest")
         }
         
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge , .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
         return true
     }
     
